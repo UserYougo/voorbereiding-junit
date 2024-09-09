fe7b217c804fadddb4989986474551850be92364
UserYougo <hugo@poelen.nl>
2024-09-09 15:06:30 +0200

    Voorbereiding les 2
    Junit FizzBuzz

--- src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
+++ src/main/java/nl/han/ica/oose/dea/testedfizzbuzz/FizzBuzzExecutor.java
@@ -2,7 +2,19 @@ package nl.han.ica.oose.dea.testedfizzbuzz;
 
 public class FizzBuzzExecutor {
 
-    public String execute(int i){
-        return null;
+    public String execute(int i) {
+        String text = "";
+        if (i % 3 == 0) {
+            text += "Fizz";
+        }
+        if (i % 5 == 0) {
+            text += "Buzz";
+        }
+        if(text == ("")){
+            text += i;
+        }
+        return text;
     }
 }
+
+
