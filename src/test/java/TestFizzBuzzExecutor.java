345e1f5d05c190d5f0e7a588a1a89653cb9ce177
UserYougo <hugo@poelen.nl>
2024-09-10 15:07:57 +0200

    BeforeEach toegevoegd

--- src/test/java/TestFizzBuzzExecutor.java
+++ src/test/java/TestFizzBuzzExecutor.java
@@ -1,43 +1,50 @@
 import nl.han.ica.oose.dea.testedfizzbuzz.FizzBuzzExecutor;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestFizzBuzzExecutor {
-    private FizzBuzzExecutor FBE;
+    private FizzBuzzExecutor sut;
+
+    @BeforeEach
+    public void setup(){
+        sut = new FizzBuzzExecutor();
+    }
 
     @Test
     void testFizz(){
         // Arrange
-        var sut = new FizzBuzzExecutor();
-
+            //setup
         // Act
         var testValue = sut.execute(21); //21%3==0 should return Fizz
 
         // Assert
-        Assertions.assertEquals("Fizz", testValue);
+        assertEquals("Fizz", testValue);
     }
     @Test
     void testBuzz(){
         //Arrange
-        var sut = new FizzBuzzExecutor();
+            //setup
 
         //Act
         var testValue = sut.execute(50);
 
         //Assert
-        Assertions.assertEquals("Buzz", testValue);
+        assertEquals("Buzz", testValue);
     }
 
     @Test
     void testFizzBuzz(){
         //Arrange
-        var sut = new FizzBuzzExecutor();
+            //setup
 
         //Act
         var testValue = sut.execute(15);
 
         //Assertion
-        Assertions.assertEquals("FizzBuzz", testValue);
+        assertEquals("FizzBuzz", testValue);
     }
 
 }
